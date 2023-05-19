ab283c65146efec298919a59e24e548cc0db870d
abhi.07on@gmail.com <abhishek@arivani.net>
2023-05-19 09:39:39 +0530

    initial commit

---
+++ pages/SplashScreen.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+
+const SplashScreen = () => {
+    return (
+        <View>
+            <Text>klbvfhjodfh</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({})
+
+export default SplashScreen;
---
+++ pages/Thanks.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import { View, StyleSheet, Image, Text } from "react-native";
+import Logo from "../assets/icon.png";
+
+const Thanks = () => {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Image style={styles.logo} source={Logo} />
+      </View>
+      <View>
+        <Text style={styles.welcome}>Welcome!</Text>
+      </View>
+
+      <View style={styles.textColor}>
+        <View>
+          <Text style={styles.registration}>Thanks for Registration.</Text>
+        </View>
+        <View>
+          <Text style={styles.verify}>Please verify your email</Text>
+        </View>
+        <View>
+          <Text style={styles.verify}>and sign in now!.</Text>
+        </View>
+      </View>
+      <View style={styles.size}>
+        <Text style={styles.back}>
+          Back to <Text style={styles.sign}>Sign in here</Text>
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#112342",
+    width: "100%",
+    height: "100%",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    padding: 50,
+  },
+  logo: {
+    height: 106,
+    width: 280,
+    top: 50,
+  },
+  welcome: {
+    fontSize: 35,
+    color: "#fff",
+    marginTop: 150,
+  },
+  registration: {
+    fontSize: 25,
+    color: "#fff",
+    marginTop: 100,
+    textAlign:"center",
+   
+    color: "#146e93",
+  },
+  verify: {
+    fontSize: 25,
+    color: "#fff",
+    textAlign: "center",
+    color: "#146e93",
+  },
+  size: { position: "absolute", bottom: 50 },
+  back: {
+    fontSize: 15,
+    color: "#146e93",
+    fontSize: 18,
+  },
+  sign: {
+    color: "#fff",
+  },
+});
+
+export default Thanks;
